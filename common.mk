a9953dd1243d1b11b59c31941f91adfa0d3f60b4
vjspranav <pranavasri@live.in>
2021-10-19 19:53:59 +0530

    Revert "sdm845-common: Switch to NXP NFC stack"
    This reverts commit 07c44c6547837d8a67eebbc703c77723352560db.

--- common.mk
+++ common.mk
@@ -283,6 +283,10 @@ PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
     vendor.nxp.hardware.nfc@2.0-service \
+    com.gsma.services.nfc \
+    QuickAccessWallet \
+    NfcNci \
+    SecureElement \
     Tag
 
 PRODUCT_COPY_FILES += \
