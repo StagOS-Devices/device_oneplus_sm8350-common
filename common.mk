e92547cbbb2b9b99fea638bc4aaa1e4767b4d03d
vjspranav <pranavasri@live.in>
2021-12-05 15:37:51 +0530

    Build vndk 30

--- common.mk
+++ common.mk
@@ -46,6 +46,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
