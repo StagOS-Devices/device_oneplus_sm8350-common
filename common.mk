119423b78adf22c86f12081b508572d47687ccc9
vjspranav <pranavasri@live.in>
2022-02-12 18:30:15 +0530

    Revert "sm8350-common: Import DeviceSettings from sm8150-common"
    This reverts commit 318eeaadb8518a54b7ac65dbf29bcc438ec92a71.

--- common.mk
+++ common.mk
@@ -255,10 +255,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
     $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
 
-# DeviceSettings
-PRODUCT_PACKAGES += \
-    DeviceSettings
-
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.common-V1-ndk_platform \
@@ -526,8 +522,11 @@ PRODUCT_PACKAGES += \
 # tri-state-key
 PRODUCT_PACKAGES += \
     KeyHandler \
-    tri-state-key-calibrate \
-    tri-state-key_daemon.vendor
+    tri-state-key-calibrate
+
+# Trust HAL
+#PRODUCT_PACKAGES += \
+    #vendor.lineage.trust@1.0-service
 
 # Update engine
 PRODUCT_PACKAGES += \
