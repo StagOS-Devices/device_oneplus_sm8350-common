9ade624e50176a354be0ec2d132b3e46c4e1e4d4
chandu078 <chandudyavanapelli03@gmail.com>
2022-04-30 12:40:38 +0000

    sm8350-common: Update blobs from LA.UM.9.14.r1-18900-LAHAINA.QSSI12.0 system: LA.QSSI.11.0.r1-14700-qssi.0
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -187,7 +187,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
-    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
@@ -257,9 +257,10 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     init.qti.display_boot.sh \
     libdisplayconfig.qti \
-    libdisplayconfig.qti.vendor \
+    libdisplayconfig.system.qti \
     libmemutils \
     libqdMetaData \
+    libqdMetaData.system \
     libsdmcore \
     libsdmutils \
     libtinyxml \
